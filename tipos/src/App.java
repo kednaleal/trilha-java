a84e55f870ddb2cacfa64a26e79d3038c7dd28e0
kednaleal <capic.2023118tads0017@aluno.ifpi.edu.br>
2023-09-06 10:25:43 -0300

    foi feito a implementação dos métodos sacar, depositar, transferir e mostrar histórico

--- tipos/src/App.java
+++ tipos/src/App.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Cliente cliente = new Cliente();
-        acessarConta();
+        Transacoes transacoes = Transacoes();
+
+        
+        transacoes.deposito(1000.0);
+        transacoes.deposito(500.0);
+        transacoes.saque(150.0);
+        transacoes.mostrarHistorico();
 
     }
 }
--- tipos/src/Cliente.java
+++ tipos/src/Cliente.java
@@ -1,16 +1,39 @@
 import java,util.LocalDate;
 
-public class Cleinte{
-    String nome;
-    String CPF = "777.333.456-00";
+public class Cliente{
+    public String nome;
+    private String CPF;
     LocalDate dataNascimento;
-    String email = "aluno@gmail.com";
-    int numero_celular = 966668899 ;
-    String endereco = "Avenida Getúlio Vagas";
-}
+    private String email;
+    private int numero_celular;
+    String endereco;
 
-public void acessarConta( String entrarNaConta){
-    System.out.println("Digite seu nome: ");
-    Scanner in = new Scanner(System.in);
-    String nome = in.nextLine();
+    public Cliente (String nome, String  cpf, String email){
+        this.nome = nome;
+        this.CPF = cpf;
+        this.email = email;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.CPF = cpf;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String emmail){
+        return email;
+    }
+    public void acessarConta( String entrarNaConta){
+        System.out.println("Digite seu nome: ");
+        Scanner in = new Scanner(System.in);
+        String nome = in.nextLine();
+    }
 }
